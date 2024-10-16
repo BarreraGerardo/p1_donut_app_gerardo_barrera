d7b6ccb381dcc768c48f1a579fd6cef7061a02b2
BarreraGerardo <gerardobguzman14@gmail.com>
2024-10-16 12:38:48 -0600

    Rochytec 2 BarreraGerardo

--- p1_donut_app_gerardo_barrera_2/lib/pages/home_page.dart
+++ p1_donut_app_gerardo_barrera_2/lib/pages/home_page.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:p1_donut_app_gerardo_barrera_2/utils/my_tab.dart';
 
-import '../utils/my_tab.dart';
+import '../tab/donut_tab.dart';
+import '../tab/burger_tab.dart';
+import '../tab/pancake_tab.dart';
+import '../tab/pizza_tab.dart';
+import '../tab/smoothie_tab.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,30 +15,18 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
-  List<Widget> myTabs = [
+List<Widget> myTabs = [
     //donut tab
-    const MyTab(
-      iconPath: 'lib/icons/donut.png',
-      ),
-    //burger tab
-     const MyTab(
-      iconPath: 'lib/icons/burger.png',
-      ),
-    //smoothie tab
-     const MyTab(
-      iconPath: 'lib/icons/smoothie.png',
-      ),
-    //pancake tab
-     const MyTab(
-      iconPath: 'lib/icons/pancakes.png',
-      ),
-    // pizza tab
-     const MyTab(
-      iconPath: 'lib/icons/pizza.png',
-      )
-  ];
+    const MyTab(iconPath: 'lib/icons/donut.png'),
+    //burguer tab
+    const MyTab(iconPath: 'lib/icons/burger.png'),
+    //smothie tab
+    const MyTab(iconPath: 'lib/icons/pizza.png'),
+    //pizza tab
+    const MyTab(iconPath: 'lib/icons/smoothie.png'),
 
+    const MyTab(iconPath: "lib/icons/pancake.png"),
+  ];
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -41,42 +34,52 @@ class _HomePageState extends State<HomePage> {
       child: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.transparent,
-          leading: 
-            Padding(
-              padding: const EdgeInsets.only(left: 24.0),
-              child: Icon(Icons.menu, 
-              color: Colors.grey[800]
-              ),
+          leading: Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: Icon(
+              Icons.menu,
+              color: Colors.grey[800],
             ),
-            actions: const [Padding(
+          ),
+          actions: const [
+            Padding(
               padding: EdgeInsets.only(right: 24.0),
               child: Icon(Icons.person),
-            )],
+            )
+          ],
         ),
         body: Column(
           children: [
-            //Texto "I want to eat"
-             const Padding(
-               padding: EdgeInsets.all(24.0),
-               child: Row(
-                 children: [
-                   Text("I want to ", style: TextStyle(fontSize: 24 ,fontWeight: FontWeight.bold,decoration: TextDecoration.underline),),
-                   Text(
-                    "Eat",
-                     style: TextStyle(
-                      fontSize: 24, 
-                      fontWeight: FontWeight.bold,
-                      decoration: TextDecoration.underline),
-                     )
-                 ],
-               ),
-             ),
+            //Texto: I want to eat
+            const Padding(
+              padding: EdgeInsets.all(24.0),
+              child: Row(
+                children: [
+                  Text("I want to  ", style: TextStyle(fontSize: 24)),
+                  Text("eat:",
+                      style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          decoration: TextDecoration.underline)),
+                ],
+              ),
+            ),
             //Tab bar
             TabBar(tabs: myTabs),
             //Tab bar view
-      
+            const Expanded(
+              child: TabBarView(children: [
+              DonutTab(),
+              BurgerTab(),
+              SmoothieTab(),
+              PancakeTab(),
+              PizzaTab()
+            ],
+            ),
             //Total del carrito
-          ],),
+            ),
+          ],
+        ),
       ),
     );
   }
